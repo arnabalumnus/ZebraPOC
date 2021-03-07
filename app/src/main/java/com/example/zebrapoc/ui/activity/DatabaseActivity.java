0da82b245289f6f63d956401f858a0aa1b5b1c8d
Arnab Kundu <68896958+arnabalumnus@users.noreply.github.com>
2021-03-07 18:34:29 +0530

    Record count in DB Table

--- app/src/main/java/com/example/zebrapoc/ui/activity/DatabaseActivity.java
+++ app/src/main/java/com/example/zebrapoc/ui/activity/DatabaseActivity.java
@@ -1,28 +1,66 @@
 package com.example.zebrapoc.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import com.example.zebrapoc.R;
 import com.example.zebrapoc.db.AppDatabase;
 
 public class DatabaseActivity extends AppCompatActivity {
 
+    private static final String TAG = "DatabaseActivity";
+    TextView tv_db_record_count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+
+        tv_db_record_count = findViewById(R.id.tv_db_record_count);
     }
 
 
     public void getAllEvent(View view) {
-        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
-                AppDatabase.class, "database-name").build();
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").build();
         Runnable runnable = () -> db.eventLogDao().getAll();
         Thread thread = new Thread(runnable);
         thread.start();
+    }
+
+    public void getTotalCount(View view) {
+        /*AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "database-name").build();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                long count = db.eventLogDao().getCount();
+                //Toast.makeText(DatabaseActivity.this, "Count: " + count, Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "run: count:" + count);
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();*/
+        new DBTask().execute();
+    }
+
+    class DBTask extends AsyncTask<Void, Void, Long> {
+
+        @Override
+        protected Long doInBackground(Void... voids) {
+            AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").build();
+            long count = db.eventLogDao().getCount();
+            return count;
+        }
+
+        @Override
+        protected void onPostExecute(Long count) {
+            super.onPostExecute(count);
+            tv_db_record_count.setText("Count: " + count);
+        }
     }
 }
