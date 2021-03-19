33d11e5db47e18ba48ccb7053ecbc896237d1fc5
Arnab Kundu <68896958+arnabalumnus@users.noreply.github.com>
2021-03-19 12:32:21 +0530

    v1.0.7 1. kotlin dependency added 2. Created kotlin folder with main, test, androidTest folder 3. Added kotlin file inside same folder 4. Exclude java and kotlin file 5. Rename app-debug.apk

---
+++ app/src/main/java/com/example/zebrapoc/utils/ExcludeClass.java
@@ -0,0 +1,10 @@
+package com.example.zebrapoc.utils;
+
+/**
+ * This class is excluded in gradle.
+ * Any code of this class will not be complied during runtime
+ */
+public class ExcludeClass {
+
+    Arnab
+}
