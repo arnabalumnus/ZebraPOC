088b7e8718ca95dabd5abf67c6cc3f8ad24c5bbf
Arnab Kundu <68896958+arnabalumnus@users.noreply.github.com>
2021-03-22 16:59:03 +0530

    v1.0.14 1. TSV graph implemented

--- app/src/main/java/com/example/zebrapoc/ui/activity/AccelerometerActivity.java
+++ app/src/main/java/com/example/zebrapoc/ui/activity/AccelerometerActivity.java
@@ -12,7 +12,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.zebrapoc.R;
-import com.example.zebrapoc.db.entity.AccLogEntity;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
@@ -25,19 +24,15 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-
-import static com.example.zebrapoc.utils.Constant.*;
+import static com.example.zebrapoc.utils.Constant.G_CALIBRATION_FACTOR;
+import static com.example.zebrapoc.utils.Constant.THROW_CALIBRATION_FACTOR;
 
 public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener, OnChartValueSelectedListener {
 
     private final String TAG = this.getClass().getSimpleName();
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
-    Queue<AccLogEntity> accLogEntityQueue = new ArrayDeque<>();
-    private LineChart chart;
+    private LineChart xyzChart, tsvChart;
     private TextView textView;
 
     @Override
@@ -49,13 +44,25 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
 
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        serGraph();
-        //feedMultiple();
+
+        xyzChart = findViewById(R.id.chart_xyz);
+        tsvChart = findViewById(R.id.chart_tsv);
+        setXYZGraph(xyzChart);
+        setTSVGraph(tsvChart);
     }
 
-    private void serGraph() {
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
-        chart = findViewById(R.id.chart1);
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    private void setXYZGraph(LineChart chart) {
+
         chart.setOnChartValueSelectedListener(this);
 
         // enable description text
@@ -108,9 +115,63 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
         rightAxis.setEnabled(false);
     }
 
+    private void setTSVGraph(LineChart chart) {
+
+        chart.setOnChartValueSelectedListener(this);
+
+        // enable description text
+        chart.getDescription().setEnabled(true);
+        chart.getDescription().setText("Accelerometer data");
+
+        // enable touch gestures
+        chart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setDrawGridBackground(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        chart.setPinchZoom(true);
+
+        // set an alternative background color
+        chart.setBackgroundColor(Color.LTGRAY);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        // add empty data
+        chart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = chart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        //l.setTypeface(tfLight);
+        l.setTextColor(Color.WHITE);
+
+        XAxis xl = chart.getXAxis();
+        //xl.setTypeface(tfLight);
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(false);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        //leftAxis.setTypeface(tfLight);
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setAxisMaximum(100f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
+    }
+
     private void addEntry(float x, float y, float z) {
 
-        LineData data = chart.getData();
+        LineData data = xyzChart.getData();
 
         if (data != null) {
 
@@ -140,14 +201,48 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
             data.notifyDataChanged();
 
             // let the chart know it's data has changed
-            chart.notifyDataSetChanged();
+            xyzChart.notifyDataSetChanged();
 
             // limit the number of visible entries
-            chart.setVisibleXRangeMaximum(20);
+            xyzChart.setVisibleXRangeMaximum(20);
             // chart.setVisibleYRange(30, AxisDependency.LEFT);
 
             // move to the latest entry
-            chart.moveViewToX(data.getEntryCount());
+            xyzChart.moveViewToX(data.getEntryCount());
+
+            // this automatically refreshes the chart (calls invalidate())
+            // chart.moveViewTo(data.getXValCount()-7, 55f,
+            // AxisDependency.LEFT);
+        }
+    }
+
+    private void addTSVEntry(float tsv) {
+
+        LineData data = tsvChart.getData();
+
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+
+            // set.addEntry(...); // can be called as well
+
+            if (set == null) {
+                set = createSet("TSV");
+                data.addDataSet(set);
+            }
+
+            data.addEntry(new Entry(set.getEntryCount(), tsv), 0);
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            tsvChart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            tsvChart.setVisibleXRangeMaximum(20);
+            // chart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            tsvChart.moveViewToX(data.getEntryCount());
 
             // this automatically refreshes the chart (calls invalidate())
             // chart.moveViewTo(data.getXValCount()-7, 55f,
@@ -170,6 +265,10 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
             set = new LineDataSet(null, "Z Data");
             set.setColor(ColorTemplate.MATERIAL_COLORS[3]);
         }
+        if (axis.equalsIgnoreCase("TSV")) {
+            set = new LineDataSet(null, "TSV Data");
+            set.setColor(ColorTemplate.getHoloBlue());
+        }
         if (set != null) {
             set.setAxisDependency(YAxis.AxisDependency.LEFT);
             set.setCircleColor(Color.WHITE);
@@ -185,158 +284,11 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
         return set;
     }
 
-    private Thread thread;
-
-    private void feedMultiple() {
-
-        if (thread != null)
-            thread.interrupt();
-
-        final Runnable runnable = new Runnable() {
-
-            @Override
-            public void run() {
-                //addEntry(0);
-            }
-        };
-
-        thread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                for (int i = 0; i < 1000; i++) {
-
-                    // Don't generate garbage runnables inside the loop.
-                    runOnUiThread(runnable);
-
-                    try {
-                        Thread.sleep(25);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-
-        thread.start();
-    }
-
-    private void setData(Queue<AccLogEntity> queue) {
-
-        ArrayList<Entry> values1 = new ArrayList<>();
-
-      /*  for (int i = 0; i < queue.size(); i++) {
-            float val = (float) (Math.random() * (range / 2f)) + 50;
-            values1.add(new Entry(i, val));
-        }
-      */
-
-
-        ArrayList<Entry> values2 = new ArrayList<>();
-
-     /*   for (int i = 0; i < count; i++) {
-            float val = (float) (Math.random() * range) + 450;
-            values2.add(new Entry(i, val));
-        }
-*/
-        ArrayList<Entry> values3 = new ArrayList<>();
-/*
-        for (int i = 0; i < count; i++) {
-            float val = (float) (Math.random() * range) + 500;
-            values3.add(new Entry(i, val));
-        }*/
-        int count = 0;
-        for (AccLogEntity item : queue) {
-            values1.add(new Entry(count, item.x));
-            values2.add(new Entry(count, item.y));
-            values3.add(new Entry(count, item.z));
-            count++;
-        }
-
-        LineDataSet set1, set2, set3;
-
-        if (chart.getData() != null &&
-                chart.getData().getDataSetCount() > 0) {
-            set1 = (LineDataSet) chart.getData().getDataSetByIndex(0);
-            set2 = (LineDataSet) chart.getData().getDataSetByIndex(1);
-            set3 = (LineDataSet) chart.getData().getDataSetByIndex(2);
-            set1.setValues(values1);
-            set2.setValues(values2);
-            set3.setValues(values3);
-            chart.getData().notifyDataChanged();
-            chart.notifyDataSetChanged();
-        } else {
-            // create a dataset and give it a type
-            set1 = new LineDataSet(values1, "DataSet 1");
-
-            set1.setAxisDependency(YAxis.AxisDependency.LEFT);
-            set1.setColor(ColorTemplate.getHoloBlue());
-            set1.setCircleColor(Color.WHITE);
-            set1.setLineWidth(2f);
-            set1.setCircleRadius(3f);
-            set1.setFillAlpha(65);
-            set1.setFillColor(ColorTemplate.getHoloBlue());
-            set1.setHighLightColor(Color.rgb(244, 117, 117));
-            set1.setDrawCircleHole(false);
-            //set1.setFillFormatter(new MyFillFormatter(0f));
-            //set1.setDrawHorizontalHighlightIndicator(false);
-            //set1.setVisible(false);
-            //set1.setCircleHoleColor(Color.WHITE);
-
-            // create a dataset and give it a type
-            set2 = new LineDataSet(values2, "DataSet 2");
-            set2.setAxisDependency(YAxis.AxisDependency.RIGHT);
-            set2.setColor(Color.RED);
-            set2.setCircleColor(Color.WHITE);
-            set2.setLineWidth(2f);
-            set2.setCircleRadius(3f);
-            set2.setFillAlpha(65);
-            set2.setFillColor(Color.RED);
-            set2.setDrawCircleHole(false);
-            set2.setHighLightColor(Color.rgb(244, 117, 117));
-            //set2.setFillFormatter(new MyFillFormatter(900f));
-
-            set3 = new LineDataSet(values3, "DataSet 3");
-            set3.setAxisDependency(YAxis.AxisDependency.RIGHT);
-            set3.setColor(Color.YELLOW);
-            set3.setCircleColor(Color.WHITE);
-            set3.setLineWidth(2f);
-            set3.setCircleRadius(3f);
-            set3.setFillAlpha(65);
-            set3.setFillColor(ColorTemplate.colorWithAlpha(Color.YELLOW, 200));
-            set3.setDrawCircleHole(false);
-            set3.setHighLightColor(Color.rgb(244, 117, 117));
-
-            // create a data object with the data sets
-            LineData data = new LineData(set1, set2, set3);
-            data.setValueTextColor(Color.WHITE);
-            data.setValueTextSize(9f);
-
-            // set data
-            chart.setData(data);
-        }
-    }
-
-
-    protected void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    protected void onPause() {
-        super.onPause();
-        mSensorManager.unregisterListener(this);
-    }
-
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 
     public void onSensorChanged(SensorEvent event) {
-        for (float i : event.values) {
-            //Log.d(TAG, "onSensorChanged: "+i);
-        }
-
         //Log.d(TAG, "onSensorChanged: Name: "+event.sensor.getName());
         //Log.d(TAG, "onSensorChanged: StringType: "+event.sensor.getStringType());
         //Log.d(TAG, "onSensorChanged: Vendor: "+event.sensor.getVendor());
@@ -349,18 +301,11 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
         float G_towards_X = event.values[0];
         float G_towards_Y = event.values[1];
         float G_towards_Z = event.values[2];
-
         addEntry(G_towards_X, G_towards_Y, G_towards_Z);
-        accLogEntityQueue.add(new AccLogEntity(System.currentTimeMillis(), G_towards_X, G_towards_Y, G_towards_Z));
-       /* if(accLogEntityQueue.size()>20){
-            accLogEntityQueue.remove();
-        setData(accLogEntityQueue);
-        }*/
-        if (accLogEntityQueue.size() == 20) {
 
-        }
-        //float total_G = event.values[0] + event.values[1] + event.values[2];
-        //Log.d(TAG, "onSensorChanged: Total: " + total_G);
+        double TSV = Math.sqrt((G_towards_X * G_towards_X) + (G_towards_Y * G_towards_Y) + (G_towards_Z * G_towards_Z));
+        addTSVEntry((float) TSV);
+
         if (Math.abs(G_towards_X) < G_CALIBRATION_FACTOR && Math.abs(G_towards_Y) < G_CALIBRATION_FACTOR && Math.abs(G_towards_Z) < G_CALIBRATION_FACTOR) {
             Log.e(TAG, "It's a free fall");
             textView.setText("It's a free fall");
