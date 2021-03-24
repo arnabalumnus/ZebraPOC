8a18d503de3e9b26ef1e874685fb9cbe2713c85a
arnab-kundu <arnab.kundu1990@gmail.com>
2021-03-24 10:34:03 +0530

    v1.0.16 1. LifeTimeService memory usage reduced

--- app/src/main/java/com/example/zebrapoc/service/LifeTimeService.java
+++ app/src/main/java/com/example/zebrapoc/service/LifeTimeService.java
@@ -6,8 +6,6 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -23,31 +21,18 @@ import androidx.core.app.NotificationCompat;
 import androidx.room.Room;
 
 import com.example.zebrapoc.R;
-import com.example.zebrapoc.broadcastReceiver.ServiceStopReceiver;
 import com.example.zebrapoc.db.AppDatabase;
 import com.example.zebrapoc.db.entity.AccLogEntity;
-import com.example.zebrapoc.db.entity.LogEntity;
 import com.example.zebrapoc.ui.activity.MainActivity;
-import com.example.zebrapoc.utils.DateFormatter;
-
-import java.io.FileDescriptor;
-import java.io.PrintWriter;
 
 public class LifeTimeService extends Service implements SensorEventListener {
 
-    private final String TAG = this.getClass().getSimpleName();
+    //private final String TAG = this.getClass().getSimpleName();
     private AppDatabase db;
-    //private EventLogEntity eventLogEntity;
     private AccLogEntity accLogEntity;
-    //private int mStartId;
     private float G_towards_X = 0;
     private float G_towards_Y = 0;
     private float G_towards_Z = 0;
-
-
-    public LifeTimeService() {
-        super();
-    }
 
     @Nullable
     @Override
@@ -58,29 +43,18 @@ public class LifeTimeService extends Service implements SensorEventListener {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         int frequency = intent.getExtras().getInt("frequency", 5);
-        Log.d(TAG, "onStartCommand: frequency=" + frequency);
+        //Log.d(TAG, "onStartCommand: frequency=" + frequency);
         SensorManager mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         Sensor mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         if (frequency == 50)
-            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_STATUS_ACCURACY_LOW);    //Value=1 , Rate=48~50/sec XT 50~51/sec
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_STATUS_ACCURACY_LOW);    //Value=1 , Frequency: ~50/sec
         else if (frequency == 15)
-            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM); //Value=2 , Rate=~15/sec XT 15/sec
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM); //Value=2 , Frequency: ~15/sec
         else if (frequency == 5)
-            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_STATUS_ACCURACY_HIGH);   //Value=3 , Rate=~15/sec , XT 5/sec
-        if (db == null)
-            db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").build();
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... voids) {
-                db.logDao().insert(new LogEntity("onStartCommand", DateFormatter.getTimeStampFileName(System.currentTimeMillis())));
-                return null;
-            }
-        }.execute();
-
-        //eventLogEntity = new EventLogEntity();
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_STATUS_ACCURACY_HIGH);   //Value=3 , Frequency: ~5/sec
+        db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").build();
         accLogEntity = new AccLogEntity();
         runAsForeground();
-        //return super.onStartCommand(intent, flags, startId);
         return START_NOT_STICKY;
     }
 
@@ -100,7 +74,6 @@ public class LifeTimeService extends Service implements SensorEventListener {
         }
 
         NotificationCompat.Builder notification = new NotificationCompat.Builder(this, "secondary")
-                .setLargeIcon(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.mipmap.ic_launcher))
                 .setNumber(0)
                 .setOngoing(true)
                 .setColorized(true)
@@ -109,11 +82,10 @@ public class LifeTimeService extends Service implements SensorEventListener {
                 .setSubText("Accelerometer is running...")
                 .setContentTitle("Zebra")
                 .setContentText("Accelerometer is running...")
-                .setNotificationSilent()
+                .setSilent(true)
                 .setContentIntent(pendingIntent);
 
         startForeground(12345, notification.build());
-        //ShortcutBadger.removeCount(this);
     }
 
     @Override
@@ -122,7 +94,7 @@ public class LifeTimeService extends Service implements SensorEventListener {
             G_towards_X = event.values[0];
             G_towards_Y = event.values[1];
             G_towards_Z = event.values[2];
-            //Log.w(TAG, "onSensorChanged: X:" + G_towards_X + " Y:" + G_towards_Y + " Z:" + G_towards_Z);
+            //Log.i(TAG, "onSensorChanged: X:" + G_towards_X + " Y:" + G_towards_Y + " Z:" + G_towards_Z);
 
             accLogEntity.setTs(System.currentTimeMillis());
             accLogEntity.setX(G_towards_X);
@@ -140,115 +112,18 @@ public class LifeTimeService extends Service implements SensorEventListener {
                 new AsyncTask<Void, Void, Void>() {
                     @Override
                     protected Void doInBackground(Void... voids) {
-                        //db.eventLogDao().insert(eventLogEntity);
                         db.accLogDao().insert(accLogEntity);
                         return null;
                     }
                 }.execute();
             }
         } catch (Exception e) {
-            Log.e(TAG, "onSensorChanged: " + e);
+            //Log.e(TAG, e.getMessage());
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-    }
-
-    @Override
-    public void onLowMemory() {
-        try {
-            new AsyncTask<Void, Void, Void>() {
-                @Override
-                protected Void doInBackground(Void... voids) {
-                    db.logDao().insert(new LogEntity("onLowMemory", DateFormatter.getTimeStampFileName(System.currentTimeMillis())));
-                    return null;
-                }
-            }.execute();
-        } catch (Exception e) {
-            Log.e(TAG, "onTrimMemory: " + e);
-        }
-        Intent intent = new Intent(getApplicationContext(), ServiceStopReceiver.class);
-        intent.setAction("com.example.LifeTimeService.stopped");
-        intent.putExtra("method", "onLowMemory()");
-        sendBroadcast(intent);
-        super.onLowMemory();
-    }
-
-    @Override
-    public void onTrimMemory(int level) {
-        try {
-            new AsyncTask<Void, Void, Void>() {
-                @Override
-                protected Void doInBackground(Void... voids) {
-                    db.logDao().insert(new LogEntity("onTrimMemory level:" + level, DateFormatter.getTimeStampFileName(System.currentTimeMillis())));
-                    return null;
-                }
-            }.execute();
-        } catch (Exception e) {
-            Log.e(TAG, "onTrimMemory: " + e);
-        }
-        Intent intent = new Intent(getApplicationContext(), ServiceStopReceiver.class);
-        intent.setAction("com.example.LifeTimeService.stopped");
-        intent.putExtra("method", "onTrimMemory() Level: " + level);
-        sendBroadcast(intent);
-        super.onTrimMemory(level);
-    }
-
-    @Override
-    public void onDestroy() {
-        try {
-            new AsyncTask<Void, Void, Void>() {
-                @Override
-                protected Void doInBackground(Void... voids) {
-                    db.logDao().insert(new LogEntity("onDestroy", DateFormatter.getTimeStampFileName(System.currentTimeMillis())));
-                    return null;
-                }
-            }.execute();
-        } catch (Exception e) {
-            Log.e(TAG, "onDestroy: " + e);
-        }
-        //db.close();
-        Intent intent = new Intent(getApplicationContext(), ServiceStopReceiver.class);
-        intent.setAction("com.example.LifeTimeService.stopped");
-        intent.putExtra("method", "onDestroy()");
-        sendBroadcast(intent);
-        super.onDestroy();
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onUnbind(Intent intent) {
-        return super.onUnbind(intent);
-    }
-
-    @Override
-    public void onRebind(Intent intent) {
-        super.onRebind(intent);
-    }
-
-    @Override
-    public void onTaskRemoved(Intent rootIntent) {
-        super.onTaskRemoved(rootIntent);
-    }
-
-    @Override
-    protected void dump(FileDescriptor fd, PrintWriter writer, String[] args) {
-        super.dump(fd, writer, args);
-    }
-
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(newBase);
     }
 }
