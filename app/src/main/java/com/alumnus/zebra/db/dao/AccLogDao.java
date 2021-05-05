7ee6dfe5f5a5dd80e349e97f2aeef1d8eb913387
Arnab Kundu <68896958+arnabalumnus@users.noreply.github.com>
2021-05-05 16:04:45 +0530

    v3.0.2 1. processDataChunk() implemented in AccLogDao

--- app/src/main/java/com/alumnus/zebra/db/dao/AccLogDao.java
+++ app/src/main/java/com/alumnus/zebra/db/dao/AccLogDao.java
@@ -4,6 +4,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Transaction;
 
 import com.alumnus.zebra.db.entity.AccLogEntity;
 
@@ -29,6 +30,32 @@ public interface AccLogDao {
 
     @Query("Select MAX(TS), MIN(TS) from accelerometer_log;")
     long[] getLastRecordTime();
+
+
+    //region Get data and clear the same from DB
+
+    /**
+     * processDataChunk() takes size as params and return List of AccLogEntity
+     *
+     * @param data_chunk_size Size of List<AccLogEntity> data
+     * @return List<AccLogEntity>
+     * @usage How to call this
+     * AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").build();
+     * List<AccLogEntity> accLogEntities = db.accLogDao().processDataChunk(10);
+     */
+    @Transaction()
+    default List<AccLogEntity> processDataChunk(int data_chunk_size) {
+        List<AccLogEntity> data = getDataChunk(data_chunk_size);
+        deleteChunk(data_chunk_size);
+        return data;
+    }
+
+    @Query("SELECT * FROM accelerometer_log ORDER BY TS ASC limit :chunk_size")
+    List<AccLogEntity> getDataChunk(int chunk_size);
+
+    @Query("DELETE FROM accelerometer_log WHERE id <=(SELECT id FROM accelerometer_log LIMIT 1 OFFSET :chunk_size-1)")
+    void deleteChunk(int chunk_size);
+    //endregion
 
     //@Query("SELECT DISTINCT TS, count(*) FROM accelerometer_log")
     //List<String> selectDistinctRecord();
