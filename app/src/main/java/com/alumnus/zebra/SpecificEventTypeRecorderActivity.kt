f34d732b128bf830fde0cb445b61ea6e5f12ad25
Arnab Kundu <68896958+arnabalumnus@users.noreply.github.com>
2021-07-22 12:22:29 +0530

    v5.0.8  - implemented runtime storage permission in SpecificEventTypeRecorderActivity

--- app/src/main/java/com/alumnus/zebra/SpecificEventTypeRecorderActivity.kt
+++ app/src/main/java/com/alumnus/zebra/SpecificEventTypeRecorderActivity.kt
@@ -1,20 +1,24 @@
 package com.alumnus.zebra
 
+import android.Manifest
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.room.Room
 import com.alumnus.zebra.db.AppDatabase
 import com.alumnus.zebra.db.entity.AccLogEntity
+import com.alumnus.zebra.machineLearning.utils.ExportFiles
 import com.alumnus.zebra.pojo.AccelerationNumericData
-import com.alumnus.zebra.utils.CsvFileOperator
-import com.alumnus.zebra.utils.DateFormatter
-import com.alumnus.zebra.utils.FolderFiles
+import com.alumnus.zebra.utils.*
 import kotlinx.android.synthetic.main.activity_specfice_event_type_recorder.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -23,12 +27,14 @@ import kotlinx.coroutines.launch
 
 class SpecificEventTypeRecorderActivity : AppCompatActivity(), SensorEventListener {
 
+    private val TAG = "SpecificEventTypeRecord"
     private var db: AppDatabase? = null
     private var accLogEntity: AccLogEntity? = null
     private var G_towards_X = 0f
     private var G_towards_Y = 0f
     private var G_towards_Z = 0f
     lateinit var job: Job
+    private lateinit var sp: SharedPreferences
 
     private var trackingUpToTime: Long = 0
     private lateinit var fileName: String
@@ -39,6 +45,15 @@ class SpecificEventTypeRecorderActivity : AppCompatActivity(), SensorEventListen
 
         db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database-name").allowMainThreadQueries().build()
         accLogEntity = AccLogEntity()
+
+        sp = getSharedPreferences(Constant.SP, MODE_PRIVATE)
+        if (isStoragePermissionGranted) {
+            if (!sp.getBoolean(Constant.isAutoStartPermissionGranted, false)) {
+                val editor = sp.edit()
+                editor.putBoolean(Constant.isAutoStartPermissionGranted, true)
+                editor.apply()
+            }
+        }
     }
 
     /**
@@ -96,5 +111,35 @@ class SpecificEventTypeRecorderActivity : AppCompatActivity(), SensorEventListen
         db!!.accLogDao().deleteAll(System.currentTimeMillis())
         Log.d("msg", "fetchRecordFromDBAndExportIntoCsvFile: $fileName ${DateFormatter.getTimeStampFileName(System.currentTimeMillis())}")
         btn_start_tracking.isEnabled = true
+    }
+
+    //permission is automatically granted on sdk<23 upon installation
+    private val isStoragePermissionGranted: Boolean
+        get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG, "Permission is granted")
+                true
+            } else {
+                Log.v(TAG, "Permission is revoked")
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+                false
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG, "Permission is granted")
+            true
+        }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.v(TAG, "Permission: " + permissions[0] + "was " + grantResults[0])
+            //resume tasks needing this permission
+            ExportFiles.prepareDataChunk(this, true)
+            if (!sp.getBoolean(Constant.isAutoStartPermissionGranted, false)) {
+                val editor = sp.edit()
+                editor.putBoolean(Constant.isAutoStartPermissionGranted, true)
+                editor.apply()
+            }
+        }
     }
 }
